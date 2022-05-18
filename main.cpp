ce82bf07961e32a2df871587c5364f6a94f0ccf4
Myles Pasetsky <mp3blue99@gmail.com>
2022-05-17 23:59:24 -0400

    try sfml stuff again

--- main.cpp
+++ main.cpp
@@ -30,6 +30,7 @@ int main(int argc, char** argv)
     gm::vec2d anchor{0, 0};
     Mouse mouse;
 
+    // float z = 1.0; 
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -68,9 +69,19 @@ int main(int argc, char** argv)
 
         mouse.update();
 
+        // sf::View view = window.getDefaultView();
+        // view.zoom(z*=0.9);
+        // window.setView(view);
+
         window.clear(sf::Color::White);
 
         sf::RectangleShape rect;
+        // rect.setPosition(view.getCenter());
+        // rect.setSize({10, 10});
+
+        // std::cout << view.getCenter() << "\n";
+        
+        // std::cout << window.mapCoordsToPixel(rect.getPosition()) << "\n";
         gm::vec2d rpos{0, 0};
         gm::vec2d rsize{10, 10};
         rect.setPosition(camera.world_to_screen(rpos).to<float>());

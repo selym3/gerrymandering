88dd649a2dbadfbe446a54d784020f7fb2026951
Myles Pasetsky <mp3blue99@gmail.com>
2022-05-26 23:30:16 -0400

    Add engine class to random

--- util/Random.hpp
+++ util/Random.hpp
@@ -4,10 +4,11 @@
 #include <random>
 #include <functional>
 
+#include <iterator>
+
 namespace Random
 {
 
-    
     template <typename T>
     inline auto getEngine(
         const T &min = std::numeric_limits<T>::min(),
@@ -20,13 +21,31 @@ namespace Random
             return std::bind(std::uniform_int_distribution<T>(min, max), twister);
         else
             return std::bind(std::uniform_real_distribution<T>(min, max), twister);
-
     }
 
-    inline auto getEngine() 
+    inline auto getEngine()
     {
-        return std::mt19937 { std::random_device{}() };
+        return std::mt19937{std::random_device{}()};
     }
+
+    class Engine
+    {
+        std::function<double()> engine = getEngine(0.0, 1.0);
+
+    public:
+        Engine() {}
+
+        double next() const { return engine(); }
+
+        template <typename T>
+        T next(const T &min, const T &max) const
+        {
+            return T{(next() * (max - min)) + min};
+        }
+
+        // template <std::input_or_output_iterator Iter>
+        // constexpr auto
+    };
 }
 
 #endif

35dd81002819a4c52f15cfeacf1ab090d36773dc
Myles Pasetsky <mp3blue99@gmail.com>
2022-06-16 14:13:02 -0400

    clean node

--- algos/node.hpp
+++ algos/node.hpp
@@ -5,12 +5,16 @@ namespace gm
 {
 
 using District = int;
+using Party = int;
 
 struct Node
 {
     District district;
     int population;
-    std::unordered_map<Party, int> populations_of_each_party;
+    std::unordered_map<Party, int> population_map;
+
+    // int get_population() const { return population; }
+    // int get_population(Party p) const { return population_map[p]; }
 };
 }
 

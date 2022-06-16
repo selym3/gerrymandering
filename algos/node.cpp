965ef420c639ec699f430c2ed2efa8b9c6c789a4
Myles Pasetsky <mp3blue99@gmail.com>
2022-06-16 19:22:30 -0400

    change Node api

---
+++ algos/node.cpp
@@ -0,0 +1,48 @@
+#include "./node.hpp"
+
+using namespace gm;
+
+
+Node::Node(District district) :
+    district { district },
+    population { 0 },
+    population_map {}
+{
+}
+
+void Node::set_district(District d1)
+{
+    district = d1;
+}
+
+District Node::get_district() const 
+{
+    return district;
+}
+
+int Node::get_population() const
+{
+    return population;
+}
+
+int Node::get_population(Party p) const
+{
+    auto iter = population_map.find(p);
+    if (iter == population_map.end())
+        return 0;
+
+    return iter->second;
+}
+
+void Node::set_population(Party p, int new_pop) 
+{
+    population -= get_population(p);
+    population_map[p] = new_pop;
+    population += new_pop;
+}
+
+void Node::clear_population()
+{
+    population = 0;
+    population_map.clear();
+}
--- algos/node.hpp
+++ algos/node.hpp
@@ -1,20 +1,32 @@
 #ifndef __NODE_HPP__
 #define __NODE_HPP__
 
+#include <unordered_map>
+
 namespace gm
 {
 
 using District = int;
 using Party = int;
 
+// TODO: use a party to account for non-voters / kids
 struct Node
 {
+    Node(District district);
+
+    void set_district(District d1);
+    District get_district() const;
+
+    int get_population() const;
+    int get_population(Party p) const;
+
+    void set_population(Party p, int population);
+    void clear_population();
+
+private:
     District district;
     int population;
     std::unordered_map<Party, int> population_map;
-
-    // int get_population() const { return population; }
-    // int get_population(Party p) const { return population_map[p]; }
 };
 }
 
